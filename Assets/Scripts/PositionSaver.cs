e4015d0ad75c1b6b36442f1ee9563eb9b6013072
Polina R <polly.poem@gmail.com>
2026-04-03 00:52:24 +0300

    PositionSaver

--- Assets/Scripts/PositionSaver.cs
+++ Assets/Scripts/PositionSaver.cs
@@ -1,25 +1,31 @@
-﻿using System;
+﻿using Assets.Scripts;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Android;
 
 namespace DefaultNamespace
 {
 	public class PositionSaver : MonoBehaviour
 	{
+		[Serializable]
 		public struct Data
 		{
 			public Vector3 Position;
 			public float Time;
 		}
 
+		[ReadOnly, Tooltip("This field can be filled through a context menu in Inspector via a \"Create File\" command.")]
 		private TextAsset _json;
 
+		[field: SerializeField, HideInInspector]
 		public List<Data> Records { get; private set; }
 
 		private void Awake()
 		{
-			//todo comment: Что будет, если в теле этого условия не сделать выход из метода?
+				//todo comment: Что будет, если в теле этого условия не сделать выход из метода?
+			//Null reference exception.
 			if (_json == null)
 			{
 				gameObject.SetActive(false);
@@ -28,21 +34,24 @@ namespace DefaultNamespace
 			}
 			
 			JsonUtility.FromJsonOverwrite(_json.text, this);
-			//todo comment: Для чего нужна эта проверка (что она позволяет избежать)?
+				//todo comment: Для чего нужна эта проверка (что она позволяет избежать)?
+			//избежать переписывания Records, загруженных из json.
 			if (Records == null)
 				Records = new List<Data>(10);
 		}
 
 		private void OnDrawGizmos()
 		{
-			//todo comment: Зачем нужны эти проверки (что они позволляют избежать)?
-			if (Records == null || Records.Count == 0) return;
+				//todo comment: Зачем нужны эти проверки (что они позволляют избежать)?
+            //избежать null reference exception и выхода за границы массива.
+            if (Records == null || Records.Count == 0) return;
 			var data = Records;
 			var prev = data[0].Position;
 			Gizmos.color = Color.green;
 			Gizmos.DrawWireSphere(prev, 0.3f);
-			//todo comment: Почему итерация начинается не с нулевого элемента?
-			for (int i = 1; i < data.Count; i++)
+				//todo comment: Почему итерация начинается не с нулевого элемента?
+            //потому что сфера для data[0].Position нарисована вне цикла (чтобы всегда можно было соединить элемент с предыдущим элементом)
+            for (int i = 1; i < data.Count; i++)
 			{
 				var curr = data[i].Position;
 				Gizmos.DrawWireSphere(curr, 0.3f);
@@ -55,10 +64,12 @@ namespace DefaultNamespace
 		[ContextMenu("Create File")]
 		private void CreateFile()
 		{
-			//todo comment: Что происходит в этой строке?
-			var stream = File.Create(Path.Combine(Application.dataPath, "Path.txt"));
-			//todo comment: Подумайте для чего нужна эта строка? (а потом проверьте догадку, закомментировав) 
-			stream.Dispose();
+				//todo comment: Что происходит в этой строке?
+			//вычисляется нужный путь к файлу Path.txt в папке проекта, создаётся файл и FileStream
+            var stream = File.Create(Path.Combine(Application.dataPath, "Path.txt"));
+				//todo comment: Подумайте для чего нужна эта строка? (а потом проверьте догадку, закомментировав)
+            //мы освобождаем FileStream, чтобы после создания другие функции смогли работать с файлом
+            stream.Dispose();
 			UnityEditor.AssetDatabase.Refresh();
 			//В Unity можно искать объекты по их типу, для этого используется префикс "t:"
 			//После нахождения, Юнити возвращает массив гуидов (которые в мета-файлах задаются, например)
@@ -70,6 +81,8 @@ namespace DefaultNamespace
 				//Этой командой можно загрузить сам ассет
 				var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>(path);
 				//todo comment: Для чего нужны эти проверки?
+				//мы ищем необходимый текстовый ассет.
+				//Ответный вопрос: пoчему мы не рассматриваем другие форматы? Если есть ассет Path.html, он перезапишется?
 				if(asset != null && asset.name == "Path")
 				{
 					_json = asset;
@@ -77,6 +90,7 @@ namespace DefaultNamespace
 					UnityEditor.AssetDatabase.SaveAssets();
 					UnityEditor.AssetDatabase.Refresh();
 					//todo comment: Почему мы здесь выходим, а не продолжаем итерироваться?
+					//нашли то, что хотели
 					return;
 				}
 			}
@@ -85,7 +99,16 @@ namespace DefaultNamespace
 		private void OnDestroy()
 		{
 			//todo logic...
-		}
+			if (_json == null)
+				return;
+            string serializedThis = JsonUtility.ToJson(this);
+			var path = UnityEditor.AssetDatabase.GetAssetPath(_json);
+			path = Path.Combine(Application.dataPath.Replace("Assets", ""), path);
+			File.WriteAllText(path, serializedThis);
+            UnityEditor.EditorUtility.SetDirty(_json);
+            UnityEditor.AssetDatabase.SaveAssets();
+            UnityEditor.AssetDatabase.Refresh();
+        }
 #endif
 	}
 }
---
+++ Assets/Scripts/ReadOnlyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+    }
+
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = true;
+        }
+    }
+#endif
+}
