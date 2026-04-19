d5d14b6dd36e0ba12cca7644a6dc5fb637fed226
Polina R <polly.poem@gmail.com>
2026-04-19 19:58:46 +0300

    it moves!

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,34 +1,58 @@
+﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Mover : MonoBehaviour
+namespace Assets.Scripts
 {
-	[SerializeField]
-	private float _moveTime = 1f;
-	[SerializeField]
-	private float _delayTime = 2f;
-	[SerializeField]
-	private Vector3[] _positions;
-
-	private IEnumerator Start()
+    internal class Mover : MonoBehaviour
     {
-		if(_positions.Length < 2) yield break;
-		int prev = 0, curr = 1;
-		var time = 0f;
-		var transform = this.transform;
-		while(true)
-		{
-			transform.position = Vector3.Lerp(_positions[prev], _positions[curr], time / _moveTime);
-			time += Time.deltaTime;
-			if(time >= _moveTime)
-			{
-				time = 0f;
-				prev = curr;
-				curr = (curr + 1) % _positions.Length;
-				yield return new WaitForSeconds(_delayTime);
-			}
-
-			yield return null;
-		}
-	}
+        [SerializeField, Tooltip("Local")]
+        private Vector3 _start;
+        [SerializeField, Tooltip("Local")]
+        private Vector3 _end;
+        //проблема - сломается при передвижении player:(
+        [SerializeField, Min(0.1f)]
+        private float _speed = 5;
+        [SerializeField, Min(0f)]
+        private float _delay = 0;
+        private Vector3 globalStart;
+        private Vector3 globalEnd;
+        private void RecalculateEnds()
+        {
+            globalStart = _start + transform.position;
+            globalEnd = _end + transform.position;
+        }
+        private IEnumerator Start()
+        {
+            RecalculateEnds();
+            transform.position = globalStart;
+            bool isForward = true;
+            while (true) {
+                yield return StartCoroutine(OneWayMovement(isForward));
+                yield return new WaitForSeconds(_delay);
+                isForward = !isForward;
+            }
+        }
+        private IEnumerator OneWayMovement(bool isForward)
+        {
+            var start = transform.position;
+            var end = isForward ? globalEnd : globalStart;
+            var duration = Vector3.Distance(start, end) / _speed;
+            var elapsed = 0f;
+            while (elapsed < duration) {
+                elapsed += Time.deltaTime;
+                var res = Vector3.MoveTowards(transform.position, end, _speed * Time.deltaTime);
+                transform.position = res;
+                yield return null;
+            }
+            transform.position = end;
+        }
+    }
 }
