75bbbf19cce7ba22835b6fd64c471f814b82cbeb
Polina R <polly.poem@gmail.com>
2026-04-26 13:00:30 +0300

    DI Zenject task

---
+++ Assets/Scripts/Zenject/SceneController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+public class SceneController : MonoBehaviour
+{
+    public void OpenMainScene() => SceneManager.LoadScene(0);
+    public void OpenGameScene() => SceneManager.LoadScene(1, LoadSceneMode.Additive);
+}
