e5fd9cc8f16411e9f779fa4838ac44aff13a671e
Polina R <polly.poem@gmail.com>
2026-04-26 19:58:40 +0300

    cell elements and methods

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,7 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Cell : MonoBehaviour
+public class Cell : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
+    public static event Action<Cell> OnPointerClickEvent;
+    private enum Plane { Select, Focus };
+    Dictionary<Plane, GameObject> _planes;
+    private GameObject Select => _planes[Plane.Select];
+    private GameObject Focus => _planes[Plane.Focus];
+    void Start()
+    {
+        _planes = new Dictionary<Plane, GameObject>();
+        foreach (Plane plane in Enum.GetValues(typeof(Plane)))
+            if (transform.Find(plane.ToString()) is Transform found && found != null)
+                _planes[plane] = found.gameObject;
+            else
+                Debug.LogError($"{plane} plane not found!");
+    }
+    public void OnPointerEnter(PointerEventData eventData) => Focus.GetOrAddComponent<MeshCollider>().enabled = true;
+    public void OnPointerExit(PointerEventData eventData) => Focus.GetOrAddComponent<MeshCollider>().enabled = false;
+    public void OnPointerClick(PointerEventData eventData) => OnPointerClickEvent.Invoke(this);
+    private void SetSelect(Material material)
+    {
+        Select.GetComponent<MeshCollider>().enabled = true;
+        Select.GetComponent<Renderer>().material = material;
+    }
+    private void ResetSelect()
+    {
+        Select.GetComponent<MeshCollider>().enabled = false;
+    }
 }
