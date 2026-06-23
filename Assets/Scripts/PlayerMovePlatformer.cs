4356db076515e083e26c048089d2abcd929ede2c
Polina R <polly.poem@gmail.com>
2026-06-24 01:16:43 +0300

    platformer character runs, jumps and rolls funnily

---
+++ Assets/Scripts/PlayerMovePlatformer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovePlatformer : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D _rigidbody;
+    private bool _isGrounded;
+    private Collider2D _сollider;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _сollider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        List<Collider2D> colliders = new();
+        ContactFilter2D filter = new ContactFilter2D();
+        _isGrounded = Physics2D.OverlapCollider(_сollider, filter, colliders) > 0;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, jumpForce);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            _rigidbody.linearVelocity = new Vector2(-moveSpeed, _rigidbody.linearVelocity.y);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            _rigidbody.linearVelocity = new Vector2(moveSpeed, _rigidbody.linearVelocity.y);
+    }
+}
