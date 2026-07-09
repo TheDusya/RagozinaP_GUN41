5791ef8dcfc3a549061c088bcbdf404939805402
Polina R <polly.poem@gmail.com>
2026-07-10 00:14:49 +0300

    ball rolls, pins fall, life is good

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,3 +1,5 @@
+using Assets.Scripts;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,10 +11,24 @@ public class BallScript : MonoBehaviour, IPointerClickHandler
     private (float min, float max) _xRange;
     [Inject(Id = "Start")]
     private GameObject _startingPoint;
+    [Inject]
+    private GameParameters _gameParameters;
     private bool _isPointChosen;
+    private Rigidbody _rigidbody;
 
     [Inject]
-    void AfterInject() => SetRandomStart();
+    void AfterInject()
+    {
+        if (!TryGetComponent<Rigidbody>(out _rigidbody))
+            throw new System.Exception("Rigidbody not found");
+        if (!TryGetComponent<Renderer>(out var renderer))
+            throw new System.Exception("Renderer not found (for some reason)");
+        renderer.SetMaterials(new List<Material>() { _gameParameters.GetMaterialByType(_gameParameters.BallType), renderer.materials[1] });
+        _rigidbody.mass = _gameParameters.GetMassByType(_gameParameters.BallType);
+        _rigidbody.useGravity = false;
+        SetRandomStart();
+    }
+
     private Vector3 VectorWithX(Vector3 position, float x) => new Vector3(x, position.y, position.z);
     private void SetXForBallAndStartingPoint(float x)
     {
@@ -22,6 +38,8 @@ public class BallScript : MonoBehaviour, IPointerClickHandler
 
     void OnDrawGizmos() //for me to see the range
     {
+        if (_isPointChosen)
+            return;
         Handles.color = Color.blue;
         Handles.DrawLine(VectorWithX(transform.position, _xRange.min), VectorWithX(transform.position, _xRange.max));
     }
@@ -33,17 +51,22 @@ public class BallScript : MonoBehaviour, IPointerClickHandler
         _isPointChosen = false;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (!_isPointChosen && Physics.Raycast(ray, out var hit, 100f) && 
-            hit.point.x >= _xRange.min && hit.point.x <= _xRange.max)
+        if (!_isPointChosen)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100f) &&
+                hit.point.x >= _xRange.min && hit.point.x <= _xRange.max)
                 SetXForBallAndStartingPoint(hit.point.x);
+        }
     }
 
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
     {
         _isPointChosen = true;
+        _rigidbody.useGravity = true;
+        _startingPoint.SetActive(false);
+        _rigidbody.AddForce(new Vector3(0, 0, 1) * _gameParameters.PunchPower, ForceMode.Impulse);
     }
 }
--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -1,21 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
-using UnityEngine.UI;
-using Zenject;
 
 namespace Assets.Scripts
 {
     internal class GameParameters : MonoBehaviour
     {
-        [Inject(Id = "Ball")]
-        GameObject _ball;
-        private Renderer _ballRenderer;
-        private Rigidbody _ballRigidbody;
+        public float PunchPower;
+        [Space(10)]
+        public BallType BallType;
 
-        [SerializeField]
-        private BallType _ballType;
         //not the prettiest decision, but better than nothing
         [SerializeField, Space(10)]
         Material _lightMaterial;
@@ -30,18 +23,7 @@ namespace Assets.Scripts
         [SerializeField]
         float _heavyMass;
 
-        [Inject]
-        private void AfterInject()
-        {
-            if (_ball == null)
-                throw new Exception("Ball not found!");
-            _ballRenderer = _ball.GetComponent<Renderer>();
-            _ballRigidbody = _ball.GetComponent<Rigidbody>();
-             _ballRenderer.SetMaterials(new List<Material>() { GetMaterialByType(_ballType), _ballRenderer.materials[1] });
-            _ballRigidbody.mass = GetMassByType(_ballType);
-        }
-
-        private Material GetMaterialByType(BallType ballType) =>
+        public Material GetMaterialByType(BallType ballType) =>
             ballType switch
             {
                 BallType.Light => _lightMaterial,
@@ -50,7 +32,7 @@ namespace Assets.Scripts
                 _ => throw new NotImplementedException()
             };
 
-        private float GetMassByType(BallType ballType) =>
+        public float GetMassByType(BallType ballType) =>
             ballType switch
             {
                 BallType.Light => _lightMass,
