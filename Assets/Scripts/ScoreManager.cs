d32d1def490ab5e9fbe972f0044b186fb0a16c6f
Polina R <polly.poem@gmail.com>
2026-07-12 19:49:38 +0300

    SCORE FINALLY WORKS AS INTENDED HURRAY HURRAY AHAHA

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,12 +2,14 @@
 
 namespace Assets.Scripts
 {
-    internal class ScoreManager : IDisposable
+    internal class ScoreManager : IDisposable //Logic is horrible here, but i am too tired of this class
     {
-        private int _currentScore;
-        private int _totalScore;
+        private int _currentScore = 0;
+        private int _totalScore = 0;
         private int _strikeBonusesLeft = 0;
         private int _spareBonusesLeft = 0;
+        private int _pinsFellCount = 0;
+        private bool _skipRoundForStrike = false;
 
         private GameParameters _gameParameters;
         private EventManager _eventManager;
@@ -15,8 +17,6 @@ namespace Assets.Scripts
 
         public ScoreManager(GameParameters gameParameters, EventManager eventManager, TextManager textManager)
         {
-            _currentScore = 0;
-            _totalScore = 0; 
             _gameParameters = gameParameters;
             _eventManager = eventManager;
             _textManager = textManager;
@@ -45,10 +45,20 @@ namespace Assets.Scripts
             _textManager.WriteBonus(bonus);
         }
 
-        private void PinFell() => SetCurrent(_currentScore + 1);
+        private void PinFell()
+        {
+            _pinsFellCount++;
+            SetCurrent(_currentScore + 1);
+        }
 
         private void HandleEndRound(bool isFirstRound)
         {
+            bool allThePinsAreDown = _pinsFellCount == _gameParameters.PinAmount;
+            if (_skipRoundForStrike)
+            {
+                _skipRoundForStrike = false;
+                return;
+            }
             if (_strikeBonusesLeft > 0)
             {
                 SetCurrent(_currentScore + _gameParameters.StrikeBonus);
@@ -60,18 +70,20 @@ namespace Assets.Scripts
                 _spareBonusesLeft--;
             }
 
-            if (isFirstRound && _currentScore == _gameParameters.PinAmount) //STRIKE
+            if (isFirstRound && allThePinsAreDown) //STRIKE
             {
+                _skipRoundForStrike = true;
                 _strikeBonusesLeft += 2;
                 _eventManager.EndRoundInvoke();
             }
-            else if (_currentScore == _gameParameters.PinAmount)
+            else if (_currentScore == _gameParameters.PinAmount && allThePinsAreDown)
                 _spareBonusesLeft += 1;
             DisplayBonuses();
         }
 
         private void HandleEndCouple()
         {
+            _pinsFellCount = 0;
             SetTotal(_totalScore + _currentScore);
             SetCurrent(0);
         }
