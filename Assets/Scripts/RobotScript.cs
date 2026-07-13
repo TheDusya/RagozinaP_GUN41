f69e05cd48bedced34476a5163b34296f66de36b
Polina R <polly.poem@gmail.com>
2026-07-14 01:02:42 +0300

    some scene formed

---
+++ Assets/Scripts/RobotScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class RobotScript : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed;
+        [SerializeField]
+        private float _maxObstacleDistance;
+    }
+}
