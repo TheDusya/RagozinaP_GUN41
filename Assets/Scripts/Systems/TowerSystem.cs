4fc33d0800be54fb3368750da00c6565ba49cdc8
Polina R <polly.poem@gmail.com>
2026-08-19 21:43:14 +0300

    tower system

--- Assets/Scripts/Systems/TowerSystem.cs
+++ Assets/Scripts/Systems/TowerSystem.cs
@@ -1,6 +1,7 @@
 ﻿using Netologia.Behaviours;
 using Netologia.TowerDefence;
 using Netologia.TowerDefence.Behaviors;
+using UnityEngine;
 using Zenject;
 
 namespace Netologia.Systems
@@ -12,8 +13,24 @@ namespace Netologia.Systems
 
 		public void ManualUpdate()
 		{
-			//todo Netologia homework 
-		}
+			foreach (var pool in this)
+				foreach (var tower in pool)
+				{
+					if (!tower.DecrementAttackReload(Time.deltaTime)) //on reload
+						return;
+					if (!tower.HasTarget)
+					{
+						var unitFound = _units.FindTarget(tower.transform.position, tower.Range);
+						if (unitFound is default(Unit)) //nothing found
+							return;
+						else 
+							tower.Target = unitFound;
+					}
+					var newProjectile = _projectiles[tower.Projectile].Get;
+                    newProjectile.PrepareData(transform.position, tower.Target, tower.Damage, tower.AttackElemental);
+					tower.Attack();
+				}
+        }
 
 		public void OnDespawnUnit(int unitID)
 		{
